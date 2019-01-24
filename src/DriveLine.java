288d1217230874b6249cb177bac2e0431e81e48e
FrankS <frank.schneidewind@maibornwolff.de>
2019-01-24 11:42:13 +0100

    trying new alg, NOT WORKING! uncomment the other block

--- src/DriveLine.java
+++ src/DriveLine.java
@@ -2,19 +2,36 @@ public class DriveLine {
 
   public static void main(String[] args) {
     AutonomerTWR roboter = new AutonomerTWR();
+    int lc, error;
+    double speedcorrector;
 
     while (true) {
       System.out.println(roboter.getLicht());
 
-      if (roboter.getLicht() > 25 && roboter.getLicht() < 50) {
+      lc = roboter.getLicht();
+      error = lc - 2;
+      speedcorrector = ((double)error / 100);
+
+      //if (roboter.getLicht() > 25 && roboter.getLicht() < 45) {
+      //  roboter.driveLeft(0.25);
+      //  roboter.driveRight(0.25);
+      //} else if (roboter.getLicht() > 45) {
+      //  roboter.driveLeft(0.25);
+      //  roboter.driveRight(0.1);
+      //} else if (roboter.getLicht() < 15) {
+      //  roboter.driveLeft(0.1);
+      //  roboter.driveRight(0.25);
+      //}
+
+      if (roboter.getLicht() > 2 && roboter.getLicht() < 45) {
         roboter.driveLeft(0.25);
         roboter.driveRight(0.25);
-      } else if (roboter.getLicht() > 50) {
-        roboter.driveLeft(0.25);
-        roboter.driveRight(0.1);
-      } else if (roboter.getLicht() < 15) {
-        roboter.driveLeft(0.1);
-        roboter.driveRight(0.25);
+      } else if (roboter.getLicht() > 45) {
+        roboter.driveLeft(speedcorrector);
+        roboter.driveRight(speedcorrector / 3);
+      } else if (roboter.getLicht() < 2) {
+        roboter.driveLeft(speedcorrector / 3);
+        roboter.driveRight(speedcorrector);
       }
     }
   }
